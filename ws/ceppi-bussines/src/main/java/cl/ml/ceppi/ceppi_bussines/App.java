d9564173f39a4f01660b38847b9d434f593a4720
dmaldonadol <maldonadoleon@gmail.com>
2015-07-06 18:42:59 -0300

    vesion inicial

---
+++ ws/ceppi-bussines/src/main/java/cl/ml/ceppi/ceppi_bussines/App.java
@@ -0,0 +1,13 @@
+package cl.ml.ceppi.ceppi_bussines;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ws/ceppi-core/src/main/java/cl/ml/ceppi/ceppi_core/App.java
@@ -0,0 +1,13 @@
+package cl.ml.ceppi.ceppi_core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
